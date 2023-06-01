712f5e0afa5d4231c9639466f3057cb80a313738
tak <wontaek22@gmail.com>
2023-06-01 23:34:54 +0900

    23.06.01
    백준 1978번 문제 (약수, 배수와 소수 단계 : 소수 찾기)

---
+++ src/com/tak/study/Main.java
@@ -0,0 +1,35 @@
+package com.tak.study;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int chk = sc.nextInt();
+        int cnt = 0;
+        int total = 0;
+
+        for (int i = 0; i < chk; i++) {
+            int a = sc.nextInt();
+
+            for (int j = 2; j <= a; j++) {
+                if (a % j == 0) {
+                    cnt++;
+                }
+            }
+
+            if (cnt == 1) {
+                total++;
+            }
+
+            cnt =0;
+        }
+
+        System.out.println(total);
+
+    }
+
+}
